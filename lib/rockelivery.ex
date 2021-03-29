2f77805b6a74650dc1ee8451e50aacd004b7d809
Matheus Campos da Silva <matheus.campos@mesainc.com.br>
2021-03-29 07:30:26 -0300

    feat: get users from repo

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -1,5 +1,7 @@
 defmodule Rockelivery do
   alias Rockelivery.Users.Create, as: UserCreate
+  alias Rockelivery.Users.Get, as: UserGet
 
   defdelegate create_user(params), to: UserCreate, as: :call
+  defdelegate get_userBy_id(id), to: UserGet, as: :by_id
 end
