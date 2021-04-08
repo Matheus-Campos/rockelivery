e967bbbc8285f3899067b906b258c28c9066aee5
Matheus Campos da Silva <matheus.campos@mesainc.com.br>
2021-04-08 09:44:43 -0300

    feat: fetch via cep client from env

--- lib/rockelivery/users/create.ex
+++ lib/rockelivery/users/create.ex
@@ -3,14 +3,13 @@ defmodule Rockelivery.Users.Create do
 
   alias Ecto.Changeset
   alias Rockelivery.{Error, Repo, User}
-  alias Rockelivery.ViaCep.Client
   alias Rockelivery.ViaCep.Response, as: ViaCepResponse
 
   def call(params) do
     cep = Map.get(params, "cep")
     with %Changeset{valid?: true} = changeset <- User.changeset(params),
          {:ok, %User{}} <- apply_action(changeset, :create),
-         {:ok, %ViaCepResponse{city: city, uf: uf}} <- Client.get_cep_info(cep),
+         {:ok, %ViaCepResponse{city: city, uf: uf}} <- via_cep_client().get_cep_info(cep),
          %Changeset{valid?: true} = changeset <- change(changeset, city: city, uf: uf),
          {:ok, %User{}} = result <- Repo.insert(changeset) do
       result
@@ -19,5 +18,11 @@ defmodule Rockelivery.Users.Create do
       {:error, %Error{}} = error -> error
       {:error, result} -> {:error, Error.build(:bad_request, result)}
     end
+  end
+
+  defp via_cep_client do
+    :rockelivery
+    |> Application.fetch_env!(__MODULE__)
+    |> Keyword.get(:via_cep_adapter)
   end
 end
